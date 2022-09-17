3e8dce164de274781bf9ef0e5c59ad2ee49d08b1
tarana <beshirova@hotmail.com>
2022-09-18 00:25:18 +0300

    task1

---
+++ semtask1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите число : ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int sqr = number*number;
+int sqr2 = (int)Math.Pow(number, 3);
+Console.WriteLine(sqr);
+Console.WriteLine(sqr2);
+
