eb199c0aef4e12a542383c9960a1dc1cfd9de065
tarana <beshirova@hotmail.com>
2022-09-18 13:18:26 +0300

    2 способа

--- semtask4/Program.cs
+++ semtask4/Program.cs
@@ -4,3 +4,11 @@ for  (int i = number*(-1); i <= number ; i++)
 {
     Console.Write(i + $" ");
 }
+// Console.WriteLine("введите число: ");
+// int num  = int.Parse(Console.ReadLine());
+// int count = -num;
+// while (count <= num )
+// {
+//   Console/Write($"{count} ");
+//    count ++;
+// } 
