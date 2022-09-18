92a5d5bf6143e2cadd3d812dd8fb9e957053cda2
tarana <beshirova@hotmail.com>
2022-09-18 18:21:29 +0300

    Задача 5

---
+++ semtask5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+ if ((num < 1000) & ( num > 99))
+{
+    Console.WriteLine( num % 10);
+}
+else {
+    Console.WriteLine("Число не соответсвует условиям задачи");
+}
